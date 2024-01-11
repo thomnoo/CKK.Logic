3582b228d1454ca8096cf66426bdf65d0f8bc375
thomnoo <68199491+thomnoo@users.noreply.github.com>
2024-01-10 19:45:32 -0700

    Added CKK.Web and all is working at this point

--- Models/Product.cs
+++ Models/Product.cs
@@ -9,7 +9,6 @@ namespace CKK.Logic.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        private decimal price { get; set; }
         public decimal Price { get; set; }
         public int Quantity { get; set; }
     }
